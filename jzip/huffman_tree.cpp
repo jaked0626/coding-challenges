b5bdd02d1d75a4d0e2b77b63412ddb56231d17a7
jaked0626 <jakez0626@gmail.com>
2025-01-09 19:34:54 +0900

    combined leaf node and internal node into a single class

--- jzip/huffman_tree.cpp
+++ jzip/huffman_tree.cpp
@@ -1,81 +1,34 @@
-#include <iostream>
 #include <memory>
 #include <map>
 #include <queue>
 
-// TODO: combine HuffBaseNode, HuffLeafNode, HuffInternalNode to a single class. 
-
-// interface for node 
-class HuffBaseNode
-{
-public:
-    // supply defaults so that we can use the interface in std::make_unique.
-    virtual bool is_leaf() const
-    {
-        return true;
-    }
-
-    virtual int get_weight() const
-    {
-        return 0;
-    };
-
-    virtual ~HuffBaseNode() = default;
-};
-
-class HuffLeafNode : public HuffBaseNode 
+class HuffmanTreeNode 
 {
 private:
-    char m_char {};
     int m_weight {};
+    std::shared_ptr<HuffmanTreeNode> m_left {};
+    std::shared_ptr<HuffmanTreeNode> m_right {};
+    char m_char {};
 
-public: 
-    HuffLeafNode(char char_, int weight)
+public:
+    // default constructor
+    HuffmanTreeNode()
+    {
+    }
+
+    // leaf node constructor 
+    HuffmanTreeNode(char char_, int weight)
         : m_char { char_ }
         , m_weight { weight }
     {
     }
 
-    int get_weight() const override
+    // internal node constructor
+    HuffmanTreeNode(std::shared_ptr<HuffmanTreeNode> left, std::shared_ptr<HuffmanTreeNode> right)
+        : m_left { left }
+        , m_right { right }
     {
-        return m_weight;
-    }
-
-    char get_char() const
-    {
-        return m_char;
-    }
-
-    bool is_leaf() const override
-    {
-        return true;
-    }
-};
-
-class HuffInternalNode : public HuffBaseNode 
-{
-private:
-    int m_weight {};
-    std::unique_ptr<HuffBaseNode> m_left {};
-    std::unique_ptr<HuffBaseNode> m_right {};
-
-public:
-    HuffInternalNode(std::unique_ptr<HuffBaseNode> left, std::unique_ptr<HuffBaseNode> right, int weight)
-        : m_weight { weight }
-        // move ownership of left and right nodes to the new internal node instance instead of copying. 
-        , m_left { std::move(left) }
-        , m_right { std::move(right) }
-    {
-    }
-
-    const HuffBaseNode& get_left() const
-    {
-        return *m_left;
-    }
-
-    const HuffBaseNode& get_right() const
-    {
-        return *m_right;
+        m_weight = m_left->get_weight() + m_right->get_weight();
     }
 
     int get_weight() const
@@ -85,88 +38,64 @@ public:
 
     bool is_leaf() const
     {
-        return false;
+        return (m_left == nullptr) && (m_right == nullptr);
+    }
+
+    std::shared_ptr<HuffmanTreeNode> get_left() const
+    {
+        return m_left;
+    }
+
+    std::shared_ptr<HuffmanTreeNode> get_right() const
+    {
+        return m_right;
     }
 };
 
-class HuffmanTree 
+class HuffmanTree
 {
 private:
-    std::unique_ptr<HuffBaseNode> m_root {};
+    std::shared_ptr<HuffmanTreeNode> m_root {};
+    int m_weight {};
 
 public:
-    // default
-    HuffmanTree()
-    {
-    }
-
-    // Leaf node constructor 
+    // initialize with leaf node
     HuffmanTree(char char_, int weight)
-        : m_root { std::make_unique<HuffLeafNode>(char_, weight) }
+        : m_root { std::make_shared<HuffmanTreeNode>(char_, weight) }
+        , m_weight { weight }
     {
     }
 
-    // Internal node constructor 
-    HuffmanTree(std::unique_ptr<HuffBaseNode> left, std::unique_ptr<HuffBaseNode> right, int weight)
-        : m_root { std::make_unique<HuffInternalNode>(std::move(left), std::move(right), weight) }
+    // initialize with internal node 
+    HuffmanTree(std::shared_ptr<HuffmanTreeNode> left, std::shared_ptr<HuffmanTreeNode> right)
+        : m_root { std::make_shared<HuffmanTreeNode>(left, right) }
     {
+        m_weight = m_root->get_weight();
     }
 
-    // Move constructor
-    HuffmanTree(HuffmanTree&& other) noexcept 
-        : m_root(std::move(other.m_root))
+    std::shared_ptr<HuffmanTreeNode> get_root() const
     {
+        return m_root;
     }
 
-    // Move assignment operator 
-    HuffmanTree& operator=(HuffmanTree&& other) noexcept
-    {
-        if (this != &other)
-        {
-            m_root = std::move(other.m_root);
-        }
-
-        return *this;
-    }
-
-    const HuffBaseNode& get_root() const
-    {
-        return *m_root;
-    }
-
-    const int get_weight() const 
+    int get_weight() const 
     {
         return m_root ? m_root->get_weight() : 0;
     }
 
-    friend bool operator== (const HuffmanTree& t1, const HuffmanTree& t2)
+    bool operator< (const HuffmanTree& other)
     {
-        return t1.get_weight() == t2.get_weight();
+        return get_weight() < other.get_weight();
     }
 
-    friend bool operator!= (const HuffmanTree& t1, const HuffmanTree& t2)
+    bool operator> (const HuffmanTree& other)
     {
-        return !(operator==(t1, t2));
+        return get_weight() > other.get_weight();
     }
 
-    friend bool operator< (const HuffmanTree& t1, const HuffmanTree& t2)
+    bool operator== (const HuffmanTree& other)
     {
-        return t1.get_weight() < t2.get_weight();
-    }
-
-    friend bool operator> (const HuffmanTree& t1, const HuffmanTree& t2)
-    {
-        return operator<(t2, t1);
-    }
-
-    friend bool operator<= (const HuffmanTree& t1, const HuffmanTree& t2)
-    {
-        return !(operator>(t1, t2));
-    }
-
-    friend bool operator>= (const HuffmanTree& t1, const HuffmanTree& t2)
-    {
-        return !(operator<(t1, t2));
+        return get_weight() == other.get_weight();
     }
 };
 
@@ -186,25 +115,15 @@ HuffmanTree build_tree(const std::map<char, int>& char_counts)
     // combine huffman trees
     while (min_heap.size() > 1)
     {
-        // temporarily remove const to move ownership. This is safe becuase the item is quickly popped from the queue.
-        HuffmanTree first { std::move(const_cast<HuffmanTree&>(min_heap.top())) };
+        HuffmanTree first { min_heap.top() };
         min_heap.pop();
-        HuffmanTree second { std::move(const_cast<HuffmanTree&>(min_heap.top())) };
+        HuffmanTree second { min_heap.top() };
         min_heap.pop();
-        
-        HuffmanTree combined 
-        {
-            std::make_unique<HuffBaseNode>(std::move(first.get_root())),
-            std::make_unique<HuffBaseNode>(std::move(second.get_root())),
-            first.get_weight() + second.get_weight()
-        };
-        min_heap.push(std::move(combined));
+        HuffmanTree combined { first.get_root(), second.get_root() };
+        min_heap.push(combined);
     }
 
-    HuffmanTree combined { std::move(const_cast<HuffmanTree&>(min_heap.top())) };
-    min_heap.pop();
-
-    return combined;
+    return min_heap.top();
 }
 
 int main()
@@ -213,10 +132,11 @@ int main()
     {
         { 'a', 5 },
         { 'b', 9 },
-        { 'c', 3 }
+        { 'c', 3 },
+        { 'z', 50 },
+        { 'e', 5 }
     };
 
     HuffmanTree tree { build_tree(char_counts) };
-    std::cout << tree.get_weight() << std::endl;
     return 0;
 }
